8a18446f09c636fdac5db4b1a7179635ef97b65b
mioe <mioe.boss@gmail.com>
2026-04-06 14:51:47 +0300

    wip: f12m1l1

--- f12m1l1/f12m1l1/main.swift
+++ f12m1l1/f12m1l1/main.swift
@@ -14,11 +14,13 @@ let lastName = "yellow"
 let fullName = firstName + " " + lastName
 print(fullName)
 
+
 /// Задание #2
 let computedName = "\(firstName) \(lastName)"
 let age: UInt8 = 255
 let q2Result = "Меня зовут " + computedName + ", и мне " + String(age) + " лет."
 print(q2Result)
+
 
 /// Задание #3
 let size: Int = 2
@@ -42,5 +44,73 @@ let sum: Int = strToNumber.reduce(0, +)
 let resut = "\(input.joined(separator: " + ")) = \(sum)"
 print(resut)
 
+
+print("\n\n")
+
+
 // MARK: Интерполяция строк
-/// Задание 1
+/// Задание #1
+print("Меня зовут \(computedName), и мне \(age) лет.")
+
+
+/// Задание #2
+/// claude.sonnet4.6: помог написать input валидатор
+func readDouble(_ prompt: String) -> Double {
+	while true {
+		print(prompt)
+		if let line = readLine(), let value = Double(line), value > 0 {
+			return value
+		}
+		print("\(prompt) is invalid, try again")
+	}
+}
+
+let weight = readDouble("Введите вес (кг):")
+let height = readDouble("Введите рост (м):")
+let bmi = weight / (height * height)
+print("Ваш ИМТ равен \(String(format: "%.2f", bmi)).")
+
+
+/// Задание #3
+func readInt(_ prompt: String) -> Int {
+	while true {
+		print(prompt)
+		if let line = readLine(), let value = Int(line), value > 0 {
+			return value
+		}
+		print("\(prompt) is invalid, try again")
+	}
+}
+
+print("input: productName")
+let productName = readLine()!
+let price = readDouble("input: price")
+let quantity = readInt("input: qty")
+print("Вы добавили в корзину \(quantity) шт. товара \(productName) на сумму \(price * Double(quantity)) руб.")
+
+
+print("\n\n")
+
+
+// MARK: Основные типы данных
+///Задание #1
+let int16min: Int16 = Int16.min
+let uint8200: UInt8 = 200
+let pi: Double = Double.pi
+let boolfalse: Bool = false
+let str: String = "Swift is awesome!"
+
+/// Задание #2
+let num = readInt("input: num")
+print("output: \(num * num)")
+
+/// Задание #3
+var flagIsEven = false
+let inputTestInt = readInt("input: inputTestInt")
+print("output: isEven \(inputTestInt.isMultiple(of: 2))")
+
+
+print("\n\n")
+
+
+
