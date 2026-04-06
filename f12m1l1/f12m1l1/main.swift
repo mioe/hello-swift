1f2b0574fc3b12d552d07775cccbe9a1c3bdeb5c
mioe <mioe.boss@gmail.com>
2026-04-06 14:28:41 +0300

    first blood

--- f12m1l1/f12m1l1/main.swift
+++ f12m1l1/f12m1l1/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - Конкатинация
+/// Задание #1
+let firstName = "duck"
+let lastName = "yellow"
+let fullName = firstName + " " + lastName
+print(fullName)
 
+/// Задание #2
+let computedName = "\(firstName) \(lastName)"
+let age: UInt8 = 255
+let q2Result = "Меня зовут " + computedName + ", и мне " + String(age) + " лет."
+print(q2Result)
+
+/// Задание #3
+let size: Int = 2
+var input = Array(repeating: "", count: size)
+var isValid = Array(repeating: false, count: size)
+
+for i in 0..<input.count {
+	print("input: n[\(i)]")
+	while (!isValid[i]) {
+		input[i] = readLine()!
+		isValid[i] = input[i].allSatisfy(\.isNumber)
+		if (!isValid[i]) {
+			print("n[\(i)]: \(input[i]) is invalid, try again")
+		}
+	}
+}
+
+/// Ближайший аналог в js: Array.prototype.flatMap
+let strToNumber = input.compactMap(Int.init)
+let sum: Int = strToNumber.reduce(0, +)
+let resut = "\(input.joined(separator: " + ")) = \(sum)"
+print(resut)
+
+// MARK: Интерполяция строк
+/// Задание 1
