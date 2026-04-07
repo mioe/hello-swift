c072ba08bab739691f56c8d446066e96635462e9
mioe <mioe.boss@gmail.com>
2026-04-07 13:10:35 +0300

    fix fmt

--- f12m1l3/f12m1l3/main.swift
+++ f12m1l3/f12m1l3/main.swift
@@ -14,7 +14,7 @@ import Foundation
  */
 var a: [Int] = [1, 2, 3, 4, 5]
 for (idx, aa) in a.enumerated() {  // for-in по массиву не даёт tuple как в js -> fix: enumerated()
-  print("a[\(idx)]: \(aa)")  // ТАКЖЕ! обрати внимание что idx является первым агрументом
+	print("a[\(idx)]: \(aa)")  // ТАКЖЕ! обрати внимание что idx является первым агрументом
 }
 
 /*
@@ -44,7 +44,7 @@ print(a[0], a[a.count - 1])
  */
 var sum = 0
 for aa in a {
-  sum += aa
+	sum += aa
 }
 print(sum)
 print(a.reduce(0, +))  // сахар
@@ -56,15 +56,15 @@ print(a.reduce(0, +))  // сахар
  */
 let randBool: Bool = .random()  // === let randBool: Bool = (Int.random(in: 0...1) == 1)
 if randBool {
-  let idx = Int.random(in: 0...a.count)
-  a.insert(10, at: idx)
-  print("🫪: a.insert(10, at: \(idx))")
+	let idx = Int.random(in: 0...a.count)
+	a.insert(10, at: idx)
+	print("🫪: a.insert(10, at: \(idx))")
 }
 let fIdx = a.firstIndex(of: 10)
 if let fIdx {
-  print("10 -> a[\(fIdx)] (by firstIndex)")
+	print("10 -> a[\(fIdx)] (by firstIndex)")
 } else {
-  print("idx not found (by firstIndex)")
+	print("idx not found (by firstIndex)")
 }
 print((a.contains(10) ? "gotcha" : "idx not found") + " (by contains)")
 
@@ -116,22 +116,22 @@ print("₹: \("₹".count)")
 
 print("🍏 utf8: \("🍏".utf8.map { $0 })")
 print("🫪 utf8: \("🫪".utf8.map { $0 })")
-print("₹ utf8: \("₹".utf8.map { $0 })")
+print("₹ utf8: \("₹".utf8.map { $0 })")ß
 
 print("🍏 utf8 count: \("🍏".utf8.count)")  // 4
 print("🫪 utf8 count: \("🫪".utf8.count)")  // 7
 print("₹ utf8 count: \("₹".utf8.count)")  // 7
 
 for word in fruits {
-  if word.count > 5 {
-    print(word)
-  }
+	if word.count > 5 {
+		print(word)
+	}
 }
 
 for word in fruits {
-  if word.utf8.count > 3 {
-    print(word + " (utf8)")
-  }
+	if word.utf8.count > 3 {
+		print(word + " (utf8)")
+	}
 }
 
 /*
@@ -151,32 +151,32 @@ print(arr)
 print("-------")
 
 for ez in arr {
-  let simpleCleanStr =
-    ez
-    .lowercased()
-    .replacingOccurrences(of: " ", with: "")
-    .trimmingCharacters(in: .whitespaces)
+	let simpleCleanStr =
+		ez
+		.lowercased()
+		.replacingOccurrences(of: " ", with: "")
+		.trimmingCharacters(in: .whitespaces)
 
-  let isPalindrome = simpleCleanStr == String(simpleCleanStr.reversed())
-  let ab = simpleCleanStr
-  let ba = String(simpleCleanStr.reversed())
-  print("\(ab) === \(ba) → \(isPalindrome ? "-> yes" : "-> no")")
+	let isPalindrome = simpleCleanStr == String(simpleCleanStr.reversed())
+	let ab = simpleCleanStr
+	let ba = String(simpleCleanStr.reversed())
+	print("\(ab) === \(ba) → \(isPalindrome ? "-> yes" : "-> no")")
 }
 
 print("-------")
 
 for entity in arr {
-  let trimAndSymbolFreeEnity =
-    entity
-    .lowercased()
-    .unicodeScalars  // убивает чистый String -> становиться String.UnicodeScalarView
-    .filter { $0.properties.isAlphabetic || $0.properties.numericType != nil }
-    .map(Character.init)  // у String.UnicodeScalarView нет == и reversed() (не могу сравнить или развернуть)
+	let trimAndSymbolFreeEnity =
+		entity
+		.lowercased()
+		.unicodeScalars  // убивает чистый String -> становиться String.UnicodeScalarView
+		.filter { $0.properties.isAlphabetic || $0.properties.numericType != nil }
+		.map(Character.init)  // у String.UnicodeScalarView нет == и reversed() (не могу сравнить или развернуть)
 
-  let isPalindrome = trimAndSymbolFreeEnity == trimAndSymbolFreeEnity.reversed()
-  let ab = String(trimAndSymbolFreeEnity)
-  let ba = String(trimAndSymbolFreeEnity.reversed())
-  print("\(ab) === \(ba) → \(isPalindrome ? "-> yes" : "-> no")")
+	let isPalindrome = trimAndSymbolFreeEnity == trimAndSymbolFreeEnity.reversed()
+	let ab = String(trimAndSymbolFreeEnity)
+	let ba = String(trimAndSymbolFreeEnity.reversed())
+	print("\(ab) === \(ba) → \(isPalindrome ? "-> yes" : "-> no")")
 }
 
 /*
@@ -188,7 +188,7 @@ let duplicates = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 1, 5, 5, 5, 5, 5]  // [1..1] - 2
 /// через reduce
 /// > https://developer.apple.com/documentation/swift/string/reduce(into:_:)#discussion
 let tryReduce = duplicates.reduce(into: [Int: Int]()) { uniqEl, el in
-  uniqEl[el, default: 0] += 1  // сахар: можно ставить дефолтное значения
+	uniqEl[el, default: 0] += 1  // сахар: можно ставить дефолтное значения
 }
 print(tryReduce)
 
@@ -197,9 +197,9 @@ var i = 0
 var whileBuffer = [Int: Int]()
 
 while i < duplicates.count {
-  let uniqEl = duplicates[i]
-  whileBuffer[uniqEl, default: 0] += 1
-  i += 1
+	let uniqEl = duplicates[i]
+	whileBuffer[uniqEl, default: 0] += 1
+	i += 1
 }
 print(whileBuffer)
 
