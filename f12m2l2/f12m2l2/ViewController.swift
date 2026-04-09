3eac633f26846abe8f0a468a791f49648cecb32d
mioe <mioe.boss@gmail.com>
2026-04-09 19:53:40 +0300

    feat/f12m2l2: done

--- f12m2l2/f12m2l2/ViewController.swift
+++ f12m2l2/f12m2l2/ViewController.swift
@@ -4,20 +4,115 @@ import UIKit
 
 class ViewController: UIViewController {
 	let hField: CGFloat = 44
-	let xPadding = 16
+	let xPadding: CGFloat = 16
+	lazy var rounded: CGFloat = hField / 2
 
 	lazy var emailField = initField(placeholder: "Email", type: .email)
 	lazy var passwordField = initField(placeholder: "Password", type: .password)
-	lazy var topicField = initField(placeholder: "Topic", type: .text)
+	lazy var topicField = initField(placeholder: "Тема", type: .text)
+
+	lazy var picture: UIImageView = {
+		let pictureView = UIImageView()
+		pictureView.translatesAutoresizingMaskIntoConstraints = false
+		pictureView.image = .homemac
+		pictureView.contentMode = .scaleAspectFit
+		pictureView.clipsToBounds = true
+		pictureView.layer.cornerRadius = rounded * 1.5
+		return pictureView
+	}()
+
+	lazy var bg: UIImageView = {
+		let pictureView = UIImageView()
+		pictureView.translatesAutoresizingMaskIntoConstraints = false
+		pictureView.image = .homemac
+		pictureView.contentMode = .scaleAspectFill
+		pictureView.clipsToBounds = true
+
+		// blur поверх картинки
+		let blur = UIVisualEffectView(
+			effect: UIBlurEffect(style: .systemUltraThinMaterial)
+		)
+		blur.frame = pictureView.bounds
+		blur.autoresizingMask = [.flexibleWidth, .flexibleHeight]  // растягивается вместе с pictureView
+		pictureView.addSubview(blur)
+
+		return pictureView
+	}()
+
+	lazy var label: UILabel = {
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Сообщение"
+		label.font = .systemFont(ofSize: 24, weight: .medium)
+		return label
+	}()
+
+	lazy var textarea: UIView = {
+		let group = UIView()
+		group.translatesAutoresizingMaskIntoConstraints = false
+
+		let wrapper = UIView()
+		wrapper.translatesAutoresizingMaskIntoConstraints = false
+		wrapper.backgroundColor = .white
+		wrapper.layer.cornerRadius = rounded * 1.5
+		wrapper.clipsToBounds = true
+
+		let label = UILabel()
+		label.translatesAutoresizingMaskIntoConstraints = false
+		label.text = "Содержимое"
+		label.font = .systemFont(ofSize: 14, weight: .regular)
+
+		let textarea = UITextView()
+		textarea.translatesAutoresizingMaskIntoConstraints = false
+		textarea.font = .systemFont(ofSize: 18, weight: .regular)
+
+		group.addSubview(label)
+		wrapper.addSubview(textarea)
+		group.addSubview(wrapper)
+
+		NSLayoutConstraint.activate([
+			label.topAnchor.constraint(equalTo: group.topAnchor),
+			label.leadingAnchor.constraint(
+				equalTo: group.leadingAnchor,
+				constant: 16
+			),
+
+			wrapper.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 8),
+			wrapper.leadingAnchor.constraint(equalTo: group.leadingAnchor),
+			wrapper.trailingAnchor.constraint(equalTo: group.trailingAnchor),
+			wrapper.bottomAnchor.constraint(equalTo: group.bottomAnchor),
+
+			textarea.topAnchor.constraint(equalTo: wrapper.topAnchor, constant: 16),
+			textarea.leadingAnchor.constraint(equalTo: wrapper.leadingAnchor, constant: 16),
+			textarea.trailingAnchor.constraint(equalTo: wrapper.trailingAnchor, constant: -16),
+			textarea.bottomAnchor.constraint(equalTo: wrapper.bottomAnchor),
+		])
+
+		return group
+	}()
+
+	lazy var button: UIButton = {
+		let button = UIButton()
+		button.translatesAutoresizingMaskIntoConstraints = false
+		button.setTitle("Отправить", for: .normal)
+		button.setTitleColor(.white, for: .normal)
+		button.backgroundColor = .black
+		button.layer.cornerRadius = rounded
+		button.heightAnchor.constraint(equalToConstant: hField).isActive = true
+		return button
+	}()
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
-		view.backgroundColor = .red
-
+		view.addSubview(bg)
+		view.addSubview(picture)
+		view.addSubview(label)
 		view.addSubview(emailField)
 		view.addSubview(passwordField)
 		view.addSubview(topicField)
+		view.addSubview(textarea)
+		view.addSubview(button)
 
 		//		emailField.heightAnchor.constraint(equalToConstant: hField).isActive = true
 		//		emailField.topAnchor.constraint(
@@ -34,9 +129,35 @@ class ViewController: UIViewController {
 		//		).isActive = true
 
 		NSLayoutConstraint.activate([
+			// bg
+			bg.topAnchor.constraint(equalTo: view.topAnchor),
+			bg.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			bg.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+			bg.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+
+			// picture
+			picture.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+			picture.leadingAnchor.constraint(
+				equalTo: view.leadingAnchor,
+				constant: 32
+			),
+			picture.trailingAnchor.constraint(
+				equalTo: view.trailingAnchor,
+				constant: -32
+			),
+			picture.heightAnchor.constraint(equalToConstant: hField * 4),
+
+			// label
+			label.topAnchor.constraint(equalTo: picture.bottomAnchor, constant: 16),
+			label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 32),
+			label.trailingAnchor.constraint(
+				equalTo: view.trailingAnchor,
+				constant: -32
+			),
+
 			// emailField
 			emailField.topAnchor.constraint(
-				equalTo: view.safeAreaLayoutGuide.topAnchor,
+				equalTo: label.bottomAnchor,
 				constant: 16
 			),
 			emailField.leadingAnchor.constraint(
@@ -50,15 +171,15 @@ class ViewController: UIViewController {
 			emailField.heightAnchor.constraint(equalToConstant: hField),
 
 			// passwordField
-			passwordField.topAnchor.constraint(equalTo: emailField.topAnchor), // на одном уровне с emailField
+			passwordField.topAnchor.constraint(equalTo: emailField.topAnchor),  // на одном уровне с emailField
 			passwordField.trailingAnchor.constraint(
 				equalTo: view.trailingAnchor,
 				constant: -32
 			),
 			passwordField.widthAnchor.constraint(equalTo: emailField.widthAnchor),  // занимает ровно столько же сколько и emailField
 			passwordField.heightAnchor.constraint(equalToConstant: hField),
-			
-			// topic
+
+			// topicField
 			topicField.topAnchor.constraint(
 				equalTo: emailField.bottomAnchor,
 				constant: 16
@@ -72,6 +193,35 @@ class ViewController: UIViewController {
 				constant: -32
 			),
 			topicField.heightAnchor.constraint(equalToConstant: hField),
+
+			// textarea
+			textarea.topAnchor.constraint(
+				equalTo: topicField.bottomAnchor,
+				constant: 16
+			),
+			textarea.leadingAnchor.constraint(
+				equalTo: view.leadingAnchor,
+				constant: 32
+			),
+			textarea.trailingAnchor.constraint(
+				equalTo: view.trailingAnchor,
+				constant: -32
+			),
+			textarea.heightAnchor.constraint(equalToConstant: hField * 4),
+
+			// button
+			button.bottomAnchor.constraint(
+				equalTo: view.bottomAnchor,
+				constant: -32
+			),
+			button.leadingAnchor.constraint(
+				equalTo: view.leadingAnchor,
+				constant: 32
+			),
+			button.trailingAnchor.constraint(
+				equalTo: view.trailingAnchor,
+				constant: -32
+			),
 		])
 	}
 
@@ -89,7 +239,7 @@ class ViewController: UIViewController {
 
 		field.placeholder = placeholder
 		field.backgroundColor = .white
-		field.layer.cornerRadius = hField / 2
+		field.layer.cornerRadius = rounded
 		field.leftView = UIView(
 			frame: CGRect(x: 0, y: 0, width: xPadding, height: 0)
 		)
