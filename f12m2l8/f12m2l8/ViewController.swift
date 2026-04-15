78b6b9ebea5eb22aa0ed40184d131819f4fa8675
mioe <mioe.boss@gmail.com>
2026-04-15 10:38:05 +0300

    f12m2l8: init

---
+++ f12m2l8/f12m2l8/ViewController.swift
@@ -0,0 +1,14 @@
+// by mioe
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
