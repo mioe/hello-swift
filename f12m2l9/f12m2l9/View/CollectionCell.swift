372dd49a30c56160e286589b8362d1e39a953811
mioe <mioe.boss@gmail.com>
2026-04-16 14:23:18 +0300

    f12m2l9: done

--- f12m2l9/f12m2l9/View/CollectionCell.swift
+++ f12m2l9/f12m2l9/View/CollectionCell.swift
@@ -7,20 +7,29 @@ class CollectionCell: UICollectionViewCell {
 	static let cellId = "CollectionCell"
 
 	private var widthConstraint: NSLayoutConstraint?
-	
+
 	private lazy var borderBottomView: UIView = {
 		$0.translatesAutoresizingMaskIntoConstraints = false
 		$0.heightAnchor.constraint(equalToConstant: 1).isActive = true
 		$0.backgroundColor = .separator
 		return $0
 	}(UIView())
-	
+
 	private lazy var stackContentView: UIStackView = {
 		$0.translatesAutoresizingMaskIntoConstraints = false
 		$0.axis = .vertical
-		$0.spacing = 16
+		$0.spacing = 8
 		return $0
 	}(UIStackView())
+
+	private lazy var avatarView: UIImageView = {
+		$0.translatesAutoresizingMaskIntoConstraints = false
+		$0.widthAnchor.constraint(equalToConstant: 48).isActive = true
+		$0.heightAnchor.constraint(equalToConstant: 48).isActive = true
+		$0.layer.cornerRadius = 24
+		$0.clipsToBounds = true
+		return $0
+	}(UIImageView())
 
 	override init(frame: CGRect) {
 		super.init(frame: frame)
@@ -38,7 +47,9 @@ class CollectionCell: UICollectionViewCell {
 			widthConstraint?.isActive = true
 		}
 		widthConstraint?.constant = collectionWidth
-		
+
+		avatarView.image = UIImage(named: tweet.user.avatar)
+
 		let tweetHeader = TweetHeaderView(
 			username: tweet.user.username,
 			nickname: tweet.user.nickname,
@@ -51,7 +62,7 @@ class CollectionCell: UICollectionViewCell {
 			media: tweet.media,
 			visualType: .collection
 		)
-		
+
 		let tweetFooter = TweetFooterView(
 			comments: tweet.comments,
 			retweets: tweet.retweets,
@@ -61,18 +72,50 @@ class CollectionCell: UICollectionViewCell {
 			visualType: .collection
 		)
 
+		stackContentView.arrangedSubviews.forEach { $0.removeFromSuperview() }
 		stackContentView.addArrangedSubview(tweetHeader)
 		stackContentView.addArrangedSubview(tweetBody)
 		stackContentView.addArrangedSubview(tweetFooter)
-		
+
+		contentView.addSubview(avatarView)
 		contentView.addSubview(stackContentView)
 		contentView.addSubview(borderBottomView)
 
 		NSLayoutConstraint.activate([
-			stackContentView.topAnchor.constraint(equalTo: contentView.topAnchor),
-			stackContentView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-			stackContentView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-			stackContentView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+			avatarView.topAnchor.constraint(
+				equalTo: contentView.topAnchor,
+				constant: 12
+			),
+			avatarView.leadingAnchor.constraint(
+				equalTo: contentView.leadingAnchor,
+				constant: 32
+			),
+
+			stackContentView.topAnchor.constraint(equalTo: avatarView.topAnchor),
+			stackContentView.leadingAnchor.constraint(
+				equalTo: avatarView.trailingAnchor,
+				constant: 8
+			),
+			stackContentView.trailingAnchor.constraint(
+				equalTo: contentView.trailingAnchor,
+				constant: -32
+			),
+			stackContentView.bottomAnchor.constraint(
+				equalTo: contentView.bottomAnchor
+			),
+
+			borderBottomView.topAnchor.constraint(
+				equalTo: stackContentView.bottomAnchor,
+				constant: 12
+			),
+			borderBottomView.leadingAnchor.constraint(
+				equalTo: contentView.leadingAnchor,
+				constant: 16
+			),
+			borderBottomView.trailingAnchor.constraint(
+				equalTo: contentView.trailingAnchor,
+				constant: -16
+			),
 		])
 	}
 
