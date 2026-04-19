b93827b89b4ea45cf8e4fa8bbbaab99db355bae0
mioe <mioe.boss@gmail.com>
2026-04-19 21:51:05 +0300

    f12m3l1: done

--- f12m3l1/f12m3l1/ContentView.swift
+++ f12m3l1/f12m3l1/ContentView.swift
@@ -7,11 +7,19 @@ struct ContentView: View {
 	private let topLocationsButtonHeight: CGFloat = 56
 
 	var body: some View {
-		VStack(alignment: .leading, spacing: 32) {
-			HeaderView()
-			WelcomeView()
-			TopLocationsView()
+		ScrollView {
+			VStack(alignment: .leading, spacing: 32) {
+				HeaderView()
+				VStack(alignment: .leading, spacing: 16) {
+					WelcomeView()
+					TopLocationsView()
+				}
+				ExploreView()
+			}
 		}
+		.scrollClipDisabled()
+		.scrollIndicators(.hidden)
+		.padding(.top, 32)
 		.padding(.horizontal, 32)
 	}
 
@@ -112,13 +120,13 @@ struct ContentView: View {
 		}
 		.buttonStyle(.plain)
 	}
-	
+
 	@ViewBuilder
 	private func TitleForSectionView(_ title: String) -> some View {
 		Text(title)
 			.font(.system(size: 18, weight: .medium))
 	}
-	
+
 	@ViewBuilder
 	private func TopLocationsView() -> some View {
 		VStack(alignment: .leading, spacing: 16) {
@@ -144,9 +152,11 @@ struct ContentView: View {
 			.scrollIndicators(.hidden)
 		}
 	}
-	
+
 	@ViewBuilder
-	private func TopLocationsButtonView(_ image: String, _ label: String) -> some View {
+	private func TopLocationsButtonView(_ image: String, _ label: String)
+		-> some View
+	{
 		Button {
 			print("onTap: \(label)")
 		} label: {
@@ -155,10 +165,9 @@ struct ContentView: View {
 					.resizable()
 					.scaledToFill()
 					.frame(width: 40, height: 40)
-					.font(.system(size: 14))
 					.clipShape(Circle())
 				Text(label)
-					.font(.system(size: 10, weight: .medium))
+					.font(.system(size: 10, weight: .regular))
 					.foregroundStyle(.sGreyDark)
 			}
 			.frame(height: topLocationsButtonHeight)
@@ -168,5 +177,119 @@ struct ContentView: View {
 			.clipShape(Capsule())
 		}
 		.buttonStyle(.plain)
+	}
+
+	@ViewBuilder
+	private func ExploreView() -> some View {
+		VStack(alignment: .leading, spacing: 16) {
+			TitleForSectionView("Explore Nearby Estates")
+
+			HStack(spacing: 0) {
+				ExploreButtonView("img-5", "Wings Tower", 220)
+				Spacer()
+				ExploreButtonView("img-6", "Sky Dandelions", 290)
+			}
+		}
+	}
+
+	@ViewBuilder
+	private func ExploreButtonView(_ image: String, _ label: String, _ price: Int)
+		-> some View
+	{
+		Button {
+			print("onTap: \(label)")
+		} label: {
+			VStack(alignment: .leading, spacing: 10) {
+				ZStack {
+					Image(image)
+						.resizable()
+						.scaledToFill()
+						.frame(width: 144, height: 144)
+					Rectangle()
+						.fill(Color(red: 0.533, green: 0.290, blue: 0.965, opacity: 0.08))
+						.frame(width: 144, height: 144)
+					VStack {
+						HStack {
+							Spacer()
+							Button {
+								print("onTap: favorite")
+							} label: {
+								Image(systemName: "heart")
+									.foregroundStyle(.red)
+									.font(.system(size: 8))
+							}
+							.buttonStyle(.plain)
+							.frame(width: 24, height: 24)
+							.background(.sGreySoft1)
+							.clipShape(Circle())
+						}
+						.padding(.all, 8)
+						Spacer()
+						HStack {
+							VStack {
+								Image(systemName: "building.2")
+									.foregroundStyle(.white)
+									.font(.system(size: 12))
+							}
+							.buttonStyle(.plain)
+							.frame(width: 26, height: 26)
+							.background(Color(red: 0.247, green: 0.275, blue: 0.486))
+							.clipShape(RoundedRectangle(cornerRadius: 8))
+							
+							Spacer()
+							
+							HStack(spacing: 0) {
+								Text("$ \(price)")
+									.font(.system(size: 14, weight: .medium))
+								VStack {
+									Spacer()
+									Text("/month")
+										.font(.system(size: 10))
+								}
+								.padding(.bottom, 6)
+							}
+							.padding(.horizontal, 8)
+							.frame(height: 26)
+							.foregroundStyle(.white)
+							.background(Color(red: 0.247, green: 0.275, blue: 0.486))
+							.clipShape(RoundedRectangle(cornerRadius: 8))
+						}
+						.padding(.all, 8)
+					}
+				}
+				.clipShape(RoundedRectangle(cornerRadius: 16))
+				
+				VStack(alignment: .leading, spacing: 10) {
+					Text(label)
+						.font(.system(size: 14, weight: .medium))
+						.foregroundStyle(.sGreyDark)
+					HStack {
+						HStack(spacing: 2) {
+							Image(systemName: "star.fill")
+								.font(.system(size: 10))
+								.foregroundStyle(.yellow)
+							Text("4.9")
+								.font(.system(size: 8, weight: .medium))
+								.foregroundStyle(.sGreyDark)
+						}
+						HStack(spacing: 2) {
+							Image(systemName: "mappin.and.ellipse")
+								.font(.system(size: 10))
+								.foregroundStyle(.red)
+							Text("Jakarta, Indonesia")
+								.font(.system(size: 8))
+								.foregroundStyle(.sGreyDark)
+						}
+					}
+				}
+				.padding(.horizontal, 8)
+			}
+		}
+		.buttonStyle(.plain)
+		.padding(.top, 8)
+		.padding(.horizontal, 8)
+		.padding(.bottom, 16)
+		.background(.sGreySoft1)
+		.clipShape(RoundedRectangle(cornerRadius: 24))
 	}
 }
