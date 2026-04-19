9ab4d484601214920ec82b062c8a5f6b2ca2b1ca
mioe <mioe.boss@gmail.com>
2026-04-19 18:31:55 +0300

    f12m3l1: init

---
+++ f12m3l1/f12m3l1/ContentView.swift
@@ -0,0 +1,15 @@
+// by mioe
+
+import SwiftUI
+
+struct ContentView: View {
+	var body: some View {
+		VStack {
+			Image(systemName: "globe")
+				.imageScale(.large)
+				.foregroundStyle(.tint)
+			Text("Hello, world!")
+		}
+		.padding()
+	}
+}
---
+++ f12m3l1/f12m3l1/f12m3l1App.swift
@@ -0,0 +1,12 @@
+// by mioe
+
+import SwiftUI
+
+@main
+struct f12m3l1App: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
