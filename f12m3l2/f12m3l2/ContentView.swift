01df8215970e224edf8433741a3fdc92ce6e897a
mioe <mioe.boss@gmail.com>
2026-04-20 20:54:17 +0300

    f12m3l2: wip

--- f12m3l2/f12m3l2/ContentView.swift
+++ f12m3l2/f12m3l2/ContentView.swift
@@ -3,15 +3,21 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State var search = ""
+	
 	var body: some View {
 		ZStack(alignment: .top) {
 			HeaderView()
+				.zIndex(1)
 			
 			ScrollView {
-				
+				VStack {
+					SearchView(promt: $search)
+				}
 			}
 			.scrollClipDisabled()
 			.scrollIndicators(.hidden)
+			.padding(.top, 48 + 32)
 		}
 		.padding(.top, 32)
 		.padding(.horizontal, 32)
@@ -25,12 +31,12 @@ struct ContentView: View {
 			} label: {
 				Image(systemName: "chevron.backward")
 					.foregroundStyle(.sGreyDark)
+					.frame(width: 48, height: 48)
+					.background(.sGreySoft1)
+					.clipShape(Circle())
+					.shadow(color: .black.opacity(0.05), radius: 8, x: 0, y: 2)
 			}
 			.buttonStyle(.plain)
-			.frame(width: 48, height: 48)
-			.background(.sGreySoft1)
-			.clipShape(Circle())
-			.shadow(color: .black.opacity(0.05), radius: 8, x: 0, y: 2)
 			
 			Spacer()
 			
@@ -38,16 +44,43 @@ struct ContentView: View {
 				print("onTap: more")
 			} label: {
 				Image(systemName: "ellipsis")
-					.foregroundStyle(.sGreyDark)
+					.foregroundStyle(.sF12M3L2Accent)
+					.background(.background)
+					.frame(width: 40, height: 40)
+					.clipShape(RoundedRectangle(cornerRadius: 12))
+					.overlay {
+						RoundedRectangle(cornerRadius: 12)
+							.strokeBorder(.sGreySoft2, lineWidth: 1)
+					}
 			}
 			.buttonStyle(.plain)
-			.frame(width: 40, height: 40)
-			.background(.background)
-			.clipShape(RoundedRectangle(cornerRadius: 12))
-			.overlay {
-				RoundedRectangle(cornerRadius: 12)
-					.strokeBorder(.sGreySoft2, lineWidth: 1)
-			}
 		}
+	}
+	
+	@ViewBuilder
+	private func SearchView(promt: Binding<String>) -> some View {
+		HStack(spacing: 16) {
+			Image(systemName: "magnifyingglass")
+				.foregroundStyle(.sGreyDark)
+			
+			TextField("Search House, Apartment, etc", text: promt)
+				.textFieldStyle(.plain)
+				.frame(maxWidth: .infinity)
+			
+			Divider()
+			
+			Button {
+				print("onTap: voice")
+			} label: {
+				Image(systemName: "mic")
+					.foregroundStyle(.sGreyBarelyMedium)
+			}
+			.buttonStyle(.plain)
+		}
+		.padding(.horizontal, 16)
+		.padding(.vertical, 16)
+		.frame(height: 70)
+		.background(.sGreySoft1)
+		.clipShape(RoundedRectangle(cornerRadius: 24))
 	}
 }
