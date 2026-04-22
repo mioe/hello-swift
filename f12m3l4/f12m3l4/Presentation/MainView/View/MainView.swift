e357951aaf0caae16eb8f0cb3e8268cd88a7315b
mioe <mioe.boss@gmail.com>
2026-04-22 13:05:44 +0300

    f12m3l4: wip

--- f12m3l4/f12m3l4/Presentation/MainView/View/MainView.swift
+++ f12m3l4/f12m3l4/Presentation/MainView/View/MainView.swift
@@ -4,12 +4,34 @@ import SwiftUI
 
 struct MainView: View {
 	var body: some View {
-		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundStyle(.tint)
-			Text("Hello, world!")
+		GeometryReader {
+			let safeAreaInsets = $0.safeAreaInsets
+			
+			NavigationStack {
+				PullEffectScrollView(
+					dragDistance: 130,
+					actionTopPadding: safeAreaInsets.top + 32,
+					leadingAction: .init(symbol: "arrow.clockwise", action: {
+						print("Add New Tab")
+					}),
+					centerAction: .init(symbol: "plus", action: {
+						print("Refresh")
+					}),
+					trailingAction: .init(symbol: "minus", action: {
+						print("Close Tab")
+					})
+				) {
+					HStack(spacing: 0) {
+						VStack {
+							Text("Hello world")
+						}
+						Spacer()
+					}
+					.padding(.horizontal, 32)
+					.background(.red)
+				}
+				.navigationBarTitleDisplayMode(.inline)
+			}
 		}
-		.padding()
 	}
 }
