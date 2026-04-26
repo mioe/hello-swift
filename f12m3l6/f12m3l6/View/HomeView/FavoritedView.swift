73813f2324e10a8b4e09adb0135185d687d7112a
mioe <mioe.boss@gmail.com>
2026-04-26 19:02:31 +0300

    f12m3l6: done more screens

--- f12m3l6/f12m3l6/View/HomeView/FavoritedView.swift
+++ f12m3l6/f12m3l6/View/HomeView/FavoritedView.swift
@@ -1,31 +1,55 @@
 // by mioe
 
+import SwiftData
 import SwiftUI
 
 struct FavoritedView: View {
-	
+
 	@Environment(AppRouter.self) private var router
-		
+
+	@Query(filter: #Predicate<Yummy> { $0.isFeatured })
+	private var featured: [Yummy]
+
+	private let columns = [
+		GridItem(.flexible(), spacing: 16),
+		GridItem(.flexible(), spacing: 16),
+	]
+
 	var body: some View {
-		ScrollView {
-			VStack(alignment: .leading, spacing: 32) {
-				CustomNavigationView(title: "Favorited") {
-					CustomNavigationItemView(
-						icon: "arrow.left",
-						onTap: { handleOpenBack() }
-					)
+		VStack(alignment: .leading, spacing: 0) {
+			CustomNavigationView(title: "Favorited") {
+				CustomNavigationItemView(
+					icon: "arrow.left",
+					onTap: { handleOpenBack() }
+				)
+			}
+			.padding(.bottom, 16)
+			.background(.background)
+
+			ScrollView {
+				LazyVGrid(columns: columns, spacing: 16) {
+					ForEach(featured, id: \.id) { fav in
+						YummyCardView(
+							yummy: fav,
+							onTap: { handleOpenYummyDetail(fav) },
+							onTapCart: { print("onTap: cart") }
+						)
+					}
 				}
 			}
+			.scrollIndicators(.hidden)
+			.contentMargins(.bottom, 120, for: .scrollContent)
 		}
-		.scrollClipDisabled()
-		.scrollIndicators(.hidden)
 		.toolbar(.hidden, for: .navigationBar)
 		.padding(.horizontal, 32)
 		.padding(.top, 20)
-		.padding(.bottom, 64)
 	}
-	
+
 	private func handleOpenBack() {
 		router.pop()
+	}
+
+	private func handleOpenYummyDetail(_ yummy: Yummy) {
+		router.openYummyDetail(yummy.id)
 	}
 }
--- f12m3l6/f12m3l6/View/HomeView/PromotionView.swift
+++ f12m3l6/f12m3l6/View/HomeView/PromotionView.swift
@@ -1,31 +1,50 @@
 // by mioe
 
+import SwiftData
 import SwiftUI
 
 struct PromotionView: View {
-	
+
 	@Environment(AppRouter.self) private var router
-	
+
+	@Query(filter: #Predicate<Yummy> { $0.isPromoted })
+	private var promotions: [Yummy]
+
 	var body: some View {
-		ScrollView {
-			VStack(alignment: .leading, spacing: 32) {
-				CustomNavigationView(title: "Promotion") {
-					CustomNavigationItemView(
-						icon: "arrow.left",
-						onTap: { handleOpenBack() }
-					)
+		VStack(alignment: .leading, spacing: 0) {
+			CustomNavigationView(title: "Promotion") {
+				CustomNavigationItemView(
+					icon: "arrow.left",
+					onTap: { handleOpenBack() }
+				)
+			}
+			.padding(.bottom, 16)
+			.background(.background)
+
+			ScrollView {
+				LazyVStack(alignment: .leading, spacing: 24) {
+					ForEach(promotions, id: \.id) { promo in
+						PromotionCardView(
+							yummy: promo,
+							onTap: { handleOpenYummyDetail(promo) },
+							color: .sAccent
+						)
+					}
 				}
 			}
+			.scrollIndicators(.hidden)
+			.contentMargins(.bottom, 120, for: .scrollContent)
 		}
-		.scrollClipDisabled()
-		.scrollIndicators(.hidden)
 		.toolbar(.hidden, for: .navigationBar)
 		.padding(.horizontal, 32)
 		.padding(.top, 20)
-		.padding(.bottom, 64)
 	}
-	
+
 	private func handleOpenBack() {
 		router.pop()
+	}
+
+	private func handleOpenYummyDetail(_ yummy: Yummy) {
+		router.openYummyDetail(yummy.id)
 	}
 }
