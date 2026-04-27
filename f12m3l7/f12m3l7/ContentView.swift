7153246b4adbf8295f4cbbb7ed5652b36f7f72f8
mioe <mioe.boss@gmail.com>
2026-04-27 18:40:05 +0300

    f12m3l7: init uikit

--- f12m3l7/f12m3l7/ContentView.swift
+++ f12m3l7/f12m3l7/ContentView.swift
@@ -4,12 +4,65 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		VStack {
-			Image(systemName: "globe")
-				.imageScale(.large)
-				.foregroundStyle(.tint)
-			Text("Hello, world!")
+		VStack(spacing: 32) {
+			VStack {
+				UIImageRepresentable()
+					.frame(width: 48, height: 48)
+				Text("Misha Gezha")
+			}
+
+			UIButtonRepresentable(
+				title: "Subscribe",
+				action: { print("onTap: subscribe") }
+			)
+			.frame(height: 48)
 		}
-		.padding()
+		.padding(32)
 	}
+}
+
+struct UIImageRepresentable: UIViewRepresentable {
+
+	func makeUIView(context: Context) -> some UIImageView {
+		{
+			$0.image = ._1775640178368
+			// не работает, в SwiftUI размером UIViewRepresentable управляет layout-движок SwiftUI, а не внутренние констрейнты UIView. SwiftUI спрашивает у обёрнутой view её intrinsicContentSize (а у UIImageView это просто размер самой картинки) и предлагает ей это пространство.
+			//			$0.widthAnchor.constraint(equalToConstant: 48).isActive = true
+			//			$0.heightAnchor.constraint(equalToConstant: 48).isActive = true
+
+			$0.layer.cornerRadius = 24
+			$0.clipsToBounds = true
+
+			// > https://stackoverflow.com/a/59745779 - $0.contentMode = .scaleAspectFill
+			$0.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+			$0.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+			return $0
+		}(UIImageView())
+	}
+
+	func updateUIView(_ uiView: UIViewType, context: Context) {}
+}
+
+struct UIButtonRepresentable: UIViewRepresentable {
+	typealias UIViewType = UIButton
+	var title: String
+	var action: () -> Void
+
+	func makeUIView(context: Context) -> UIButton {
+		let btn = UIButton()
+		btn.setTitle(title, for: .normal)
+		btn.addAction(
+			UIAction(handler: { _ in
+				action()
+			}),
+			for: .touchUpInside
+		)
+		btn.backgroundColor = .systemOrange
+		btn.setTitleColor(.white, for: .normal)
+		btn.layer.cornerRadius = 16
+		btn.clipsToBounds = true
+		return btn
+	}
+
+	func updateUIView(_ uiView: UIViewType, context: Context) {}
 }
