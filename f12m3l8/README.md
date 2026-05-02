6e281bdd72a830499dadb460599f3e1629591b8a
mioe <mioe.boss@gmail.com>
2026-05-02 11:46:22 +0300

    f12m3l8: init - done

---
+++ f12m3l8/README.md
@@ -0,0 +1,5 @@
+# f12m3l8
+
+## output
+
+![](.docs/Screenshot%202026-05-02%20at%2011.44.25%E2%80%AFAM.webp)
---
+++ f12m3l8/f12m3l8/f12m3l8App.swift
@@ -0,0 +1,12 @@
+// by mioe
+
+import SwiftUI
+
+@main
+struct f12m3l8App: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
